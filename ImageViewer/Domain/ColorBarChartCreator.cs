bcd6f54591ae038d13574cbaeff4219b7185b53c
Alexey Zhishkevich <lexa2669847@gmail.com>
2020-11-27 23:49:13 +0300

    [!] Added logic for creating histogram images by source image data as byte array

---
+++ ImageViewer/Domain/ColorBarChartCreator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+
+namespace ImageViewer.Domain
+{
+    public class ColorBarChartCreator
+    {
+        private static int _height = 90;
+
+        private int[] _redBarChartData;
+        private int[] _greenBarChartData;
+        private int[] _blueBarChartData;
+
+        public Bitmap RedBarChart { get; private set; }
+        public Bitmap GreenBarChart { get; private set; }
+        public Bitmap BlueBarChart { get; private set; }
+
+        public void CalculateImageHistogram(byte[] image, int bitsPerPixel)
+        {
+            if (image.Length % bitsPerPixel != 0)
+                throw new ArgumentException(nameof(image));
+
+            if (bitsPerPixel == 0)
+                throw new ArgumentException(nameof(bitsPerPixel));
+
+            _redBarChartData = new int[256];
+            _greenBarChartData = new int[256];
+            _blueBarChartData = new int[256];
+
+            for (int index = 0; index < image.Length; index += bitsPerPixel)
+            {
+                ++_redBarChartData[image[index]];
+                ++_greenBarChartData[image[index + 1]];
+                ++_blueBarChartData[image[index + 2]];
+            }
+
+            RedBarChart = CreateBarChart(_redBarChartData, Color.Red);
+            GreenBarChart = CreateBarChart(_greenBarChartData, Color.Green);
+            BlueBarChart = CreateBarChart(_blueBarChartData, Color.Blue);
+        }
+
+        private Bitmap CreateBarChart(int[] colorIntensities, Color colorToFillBarChart)
+        {
+            var resultBarChart = new Bitmap(256, _height);
+
+            int maxValue = 0;
+            for (int i = 0; i < 256; ++i)
+            {
+                if (colorIntensities[i] > maxValue)
+                    maxValue = colorIntensities[i];
+            }
+
+            double point = (double)maxValue / _height;
+
+            for (int i = 0; i < 256 - 1; ++i)
+            {
+                for (int j = _height - 1; j > _height - colorIntensities[i] / point; --j)
+                {
+                    resultBarChart.SetPixel(i, j, colorToFillBarChart);
+                }
+            }
+
+            return resultBarChart;
+        }
+    }
+}
