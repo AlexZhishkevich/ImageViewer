0c892d7b8a012c7f573b8d88c610443393757133
Alexey Zhishkevich <lexa2669847@gmail.com>
2020-11-28 00:24:24 +0300

    [!] Added test project and tests for HardDriveImageProvider

---
+++ ImageViewer.Tests/HardDriveImageProviderTest.cs
@@ -0,0 +1,39 @@
+﻿using ImageViewer.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace ImageViewer.Tests
+{
+    [TestClass]
+    public class HardDriveImageProviderTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void GetImage_WhenCalledWithPathToNotExistingFile_ThrowsException()
+        {
+            // Arrange
+            var hardDriveImageProvider = new HardDriveImageProvider();
+            var pathToFile = "foo";
+
+            //Act
+            hardDriveImageProvider.GetImage(pathToFile);
+
+            //Assert - Expects exception
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void GetImage_WhenFileHasNotSupportedFormat_ThrowsException()
+        {
+            // Arrange
+            var hardDriveImageProvider = new HardDriveImageProvider();
+            var pathToFile = "ImageViewer.Tests.dll";
+
+            //Act
+            hardDriveImageProvider.GetImage(pathToFile);
+
+            //Assert - Expects exception
+        }
+    }
+}
